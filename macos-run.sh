eb0988151e46f7f8f28d0075802fdf11d83d02ff
airayoun <airayoun@outlook.com>
2022-03-22 06:32:40 +0000

    init 3

--- macos-run.sh
+++ macos-run.sh
@@ -15,19 +15,9 @@ sudo dscl . -passwd /Users/bucelicetr $1
 sudo createhomedir -c -u bucelicetr > /dev/null
 sudo dscl . -append /Groups/admin GroupMembership username
 
-#Enable VNC
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
-
-echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
-
-#Start VNC/reset changes
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
-
 #install ngrok
 brew install --cask anydesk
 
 #configure ngrok and start it
-echo my_new_password| sudo /Applications/AnyDesk.app/Contents/MacOS/AnyDesk --set-password
-echo AnyDesk ID is: /Applications/AnyDesk.app/Contents/MacOS/AnyDesk --get-id
+echo mynewpassword | sudo anydesk --set-password
+echo AnyDesk ID is: anydesk --get-id
